c5b5fde0f4dd8feccc403fbd7ee5e319dbee1ee2
didcreetsadgoku500 <43486471+didcreetsadgoku500@users.noreply.github.com>
2023-07-11 19:34:28 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View, ScrollView} from 'react-native';
 
-const j = 3411 
+const j = 3505 
  
 let listItems = []
 for (let i = 0; i < j; i++) {  // 3412 errors, 3411 works
